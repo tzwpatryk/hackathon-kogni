e546b46a062571790e601120a9d6f43001c0da18
Hubert Lagoda <lagodahubert@gmail.com>
2024-05-07 23:47:13 +0200

    dodanie audio + poprawa obraz

--- app/game_functions.py
+++ app/game_functions.py
@@ -1,9 +1,8 @@
 from app import app
 import os, random
 
-def get_random_image():
-    image_folder = os.path.join(app.root_path, 'static', 'img')
+def get_random_image(type):
+    image_folder = os.path.join(app.root_path, 'static', type)
     images = os.listdir(image_folder)
     random_image = random.choice(images)
     return random_image
-
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
-from flask import render_template, request, session
+from flask import render_template, request, session, redirect, url_for
 from app import app
 import os, random
-import game_functions
+from . import game_functions
 
 @app.route('/')
 @app.route('/index')
@@ -12,41 +12,95 @@ def index():
 def mentee():
     return render_template('mentee.html')
 
+@app.route('/game_menu')
+def game_menu():
+    return render_template('game_menu.html')
 
-emotions = ['joy', 'sadness', 'anger', 'fear', 'surprise', 'contentment', 'jealousy', 'disgust']
+
+emotions = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']
 images_used = []
 images_per_game = 3
 sesja = {"counter": 0, "score": 0}
-correct_emotion = ""
+
+sesja_audio = {"counter": 0, "score": 0}
 
 @app.route('/game', methods=['GET', 'POST'])
 def game():
+    global sesja
     global correct_emotion
+
+    if sesja_audio['counter'] >= images_per_game:
+        return redirect(url_for('new_game'))  # Przekierowanie do rozpoczęcia nowej gry
+
     if request.method == 'POST':
         user_answer = request.form.get('user_answer')
-        #correct_emotion = request.form.get('correct_emotion')
         if user_answer == correct_emotion:
             sesja['score'] += 1
-        sesja['counter'] += 1  # Zwiększ wartość counter o 1 za każdym razem, gdy formularz zostanie wysłany
+        sesja['counter'] += 1
 
         if sesja['counter'] >= images_per_game:
-            return render_template('wynik.html', score=sesja["score"], total_images=images_per_game, emotion = correct_emotion, user=user_answer)
-       
-    if sesja['counter'] < images_per_game: 
-        while True:
-            random_image = game_functions.get_random_image()
-            if random_image not in images_used:
-                images_used.append(random_image)
-                break
-        image_path = f'img/{random_image}'
-        fourth_option = random_image.split("_")[0]
-        while True:
-            random_emotions = random.sample(emotions, 3)
-            if fourth_option not in random_emotions:
-                break
+            return render_template('wynik.html', score=sesja["score"], total_images=images_per_game, emotion=correct_emotion, user=user_answer)
 
+    if sesja['counter'] < images_per_game:
+        random_thing = game_functions.get_random_image("img")
+        while random_thing in images_used:
+            random_thing = game_functions.get_random_image("img")
+        images_used.append(random_thing)
+        
+        image_path = f'img/{random_thing}'
+        fourth_option = random_thing.split(".")[0]
+        random_emotions = random.sample(emotions, 3)
         random_emotions.append(fourth_option)
+        random.shuffle(random_emotions)
+
         correct_emotion = fourth_option
-        #random.shuffle(random_emotions)
         return render_template('game.html', image_path=image_path, emotions=random_emotions, correct_emotion=fourth_option, score=sesja["score"], counter=sesja["counter"])
 
+@app.route('/new_game')
+def new_game():
+    global sesja
+    global correct_emotion
+    sesja = {"counter": 0, "score": 0}
+    correct_emotion = ""
+    return redirect(url_for('game'))
+
+
+@app.route('/audio', methods=['GET', 'POST'])
+def audio():
+    global sesja_audio
+    global correct_emotion
+
+    if sesja_audio['counter'] >= images_per_game:
+        return redirect(url_for('new_audio'))  # Przekierowanie do rozpoczęcia nowej gry
+
+    if request.method == 'POST':
+        user_answer = request.form.get('user_answer')
+        if user_answer == correct_emotion:
+            sesja_audio['score'] += 1
+        sesja_audio['counter'] += 1
+
+        if sesja_audio['counter'] >= images_per_game:
+            return render_template('wynik.html', score=sesja_audio["score"], total_images=images_per_game, emotion=correct_emotion, user=user_answer)
+
+    if sesja_audio['counter'] < images_per_game:
+        random_thing = game_functions.get_random_image("sounds")
+        while random_thing in images_used:
+            random_thing = game_functions.get_random_image("sounds")
+        images_used.append(random_thing)
+        
+        image_path = f'sounds/{random_thing}'
+        fourth_option = random_thing.split(".")[0]
+        random_emotions = random.sample(emotions, 3)
+        random_emotions.append(fourth_option)
+        random.shuffle(random_emotions)
+
+        correct_emotion = fourth_option
+        return render_template('audio.html', audio_path=image_path, emotions=random_emotions, correct_emotion=fourth_option, score=sesja_audio["score"], counter=sesja_audio["counter"])
+
+@app.route('/new_audio')
+def new_audio():
+    global sesja_audio
+    global correct_emotion
+    sesja_audio = {"counter": 0, "score": 0}
+    correct_emotion = ""
+    return redirect(url_for('audio'))
